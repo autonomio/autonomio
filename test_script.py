0c2d05c1dc1e460868e0cbcb2ca43fd4c9944d43
mikkokotila <mailme@mikkokotila.com>
2017-04-23 20:09:14 +0300

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,3 +1,13 @@
 from autonomio.commands import data, train
+
+# get the data
 tweets = data('random_tweets')
-tr = train('text','neg',tweets.head(5000))
+tweets = tweets.head(500)
+
+# run train and save model
+tr = train('text','neg',tweets)
+tr = train(1,'neg',tweets)
+tr = train([1,5],'neg',tweets)
+
+# run test
+#te = test('text',tweets,'handle','model.json')
