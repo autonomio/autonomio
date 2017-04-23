744037cb23b761893b0cf16466ab88f5df6e3bd9
mikkokotila <mailme@mikkokotila.com>
2017-04-23 13:45:16 +0300

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,4 +1,10 @@
+import os   # this is for travis only
+import matplotlib as mpl # this is for travis only
 from autonomio.commands import data, train
+
+if os.environ.get('DISPLAY','') == '': # this is for travis only
+    print('no display found. Using non-interactive Agg backend') # this is for travis only
+    mpl.use('Agg') # this is for travis only
 
 tweets = data('random_tweets')
 tr = train('text','neg',tweets.head(5000))
