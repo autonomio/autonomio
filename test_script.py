2d76539432f470b32febbd1c82c4a334cbb066fd
mikkokotila <mailme@mikkokotila.com>
2017-04-23 20:47:20 +0300

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -5,8 +5,7 @@ tweets = data('random_tweets')
 tweets = tweets.head(500)
 tr = train('text','neg',tweets,save_model='multi.json')
 tr = train(1,'neg',tweets,dims=1)
-tr = train([1,5],'neg',tweets,dims=5)
-tr = train('text','neg',tweets,dims=5)
+tr = train([1,5],'neg',tweets,dims=4)
 
 # run train and save model
 te = test('text',tweets,handle,'model.json')
