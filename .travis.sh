a00ff47b9e0469661531f39976714f4daa54755a
mikkokotila <mailme@mikkokotila.com>
2017-04-23 11:41:43 +0300

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 sudo python -m spacy download en
+
+sudo pip install --upgrade pip
+sudo pip install numpy --upgrade
+sudo pip install pandas --upgrade 
+sudo pip install spacy --upgrade
+sudo pip install tensorflow --upgrade
+sudo pip install keras --upgrade 
+sudo pip install matplotlib --upgrade
+sudo pip install seaborn --upgrade 
+sudo pip install mpld3 --upgrade
+
+sudo python -m spacy download en
