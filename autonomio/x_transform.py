3b5efad9e18a53f9a18f85ede0e8dc04d8826a98
Mikko Kotila <mailme@mikkokotila.com>
2017-07-23 17:05:28 +0300

    strange pandas/numpy bug
    - Something in x_transform I can't seem to replicate on local, so trying a couple of different things (now trying ix instead of iloc)

--- autonomio/x_transform.py
+++ autonomio/x_transform.py
@@ -14,7 +14,7 @@ def x_transform(X,data):
     if type(X) == list:
         if len(X) > 2:
             if type(X[0]) == int:
-                x = data.iloc[:,[X]]
+                x = data.ix[:,[X]]
                 status = 'ok'
 
     # for multiple column labels
