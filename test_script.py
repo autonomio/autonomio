81bcbe753416ccd62e8f5a1a0bac4440e58a5bbf
konchokdolma <aanastasia.gromova@gmail.com>
2017-08-08 12:58:29 +0300

    Fixed test script 	- added validation check

--- test_script.py
+++ test_script.py
@@ -46,9 +46,9 @@ for i in l:
         tr = train(1, 'neg', temp, dims=1, shape=i, neuron_max=1, layers=8)
         tr = train(1, 'neg', temp, dims=1, shape=i, neuron_max=9, layers=4)
         
-    if i in (l[5:6]): #long_funnel, diamond
-        tr = train(1, 'neg', temp, dims=1, shape=i, layers=6)
-        tr = train(1, 'neg', temp, dims=1, shape=i, layers=5)
+	if i in (l[5:6]): #long_funnel, diamond
+        tr = train(1, 'neg', temp, dims=1, shape=i, layers=6, validation=True)
+        tr = train(1, 'neg', temp, dims=1, shape=i, layers=5, validation=.6)
         
     elif i in (l[6:8]): #hexagon, stairs
         tr = train(1, 'neg', temp, dims=1, shape=i, layers=4)
