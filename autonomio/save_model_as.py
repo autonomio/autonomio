8a305109a25698766b80a0ca8a7aee70feafaf25
Mikko Kotila <mailme@mikkokotila.com>
2017-08-15 21:32:18 +0300

    changed the structure
    - moved load_model to its own file load_model.py
    - moved save_model to its own file save_model_as.py
    - adopted iPython display for showing pandas dataframe
    - removed prints in train_new and replaced with dataframes ex1 and ex2
    - train_new returns ex1 and ex1 as output
    - moved network_scale to ex1 with other printouts
    - cleaned some reduntant codes form train_new
    - renamed kuubio() to trainer()
    - renamed test() to predictor()
    - maybe did some other stuff as well...

---
+++ autonomio/save_model_as.py
@@ -0,0 +1,23 @@
+def save_model_as(X_num, model, save_model):
+
+    model_json = model.to_json()
+    with open(save_model+".json", "w") as json_file:
+        json_file.write(model_json)
+
+    model.save_weights(save_model+".h5")
+    print("Model" + " " + save_model + " " + "have been saved.")
+
+    k = ""
+
+    f = open(save_model+".x", "w+")
+
+    if type(X_num) == list:
+        for x in X_num:
+            k = k+str(x)+" "
+    elif type(X_num) == int:
+        k = str(X_num)
+    else:
+        k = X_num
+
+    f.write(k)
+    f.close()
