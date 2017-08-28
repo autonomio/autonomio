958aa2aef4b5ce6aa968290ce5c7446bbb2ddd6d
Mikko Kotila <mailme@mikkokotila.com>
2017-08-28 16:53:43 +0300

    added LSTM
    - added the ability to run LSTM for predicting continuous features
    - this is not integerated with train() yet, will have to think about the logic
    - added lstm_transform_data() for data prep
    - added lstm_plots() with the goal of providing a meaningful insight to the accuracy of the model

---
+++ autonomio/lstm_transform_data.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+def _lstm_load_data(data, seq_len, normalise_window):
+
+    '''
+    Helper function for lstm() that takes a series or array of
+    data and tranforms it in the way that becomes useful for the model.
+
+    '''
+
+    data = data.tolist()
+
+    sequence_length = seq_len + 1
+    result = []
+    for index in range(len(data) - sequence_length):
+        result.append(data[index: index + sequence_length])
+
+    if normalise_window:
+        result = normalise_windows(result)
+
+    result = np.array(result)
+
+    row = round(0.9 * result.shape[0])
+    train = result[:int(row), :]
+    np.random.shuffle(train)
+    x_train = train[:, :-1]
+    y_train = train[:, -1]
+    x_test = result[int(row):, :-1]
+    y_test = result[int(row):, -1]
+
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    return [x_train, y_train, x_test, y_test]
+
+
+def normalise_windows(window_data):
+    normalised_data = []
+    for window in window_data:
+        normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
+        normalised_data.append(normalised_window)
+    return normalised_data
