491f8a8263d29310e607459961acebd4fe6e85c9
Mikko Kotila <mailme@mikkokotila.com>
2017-09-01 18:44:05 +0300

    fixed test script error due to changes in code and pepified the whole file
    - pepified test_script.py
    - fixed the issue from old tests relate with new codes
    - updated version in all init files

--- test_script.py
+++ test_script.py
@@ -1,6 +1,5 @@
 from autonomio.commands import data, train, predictor, wrangler, hyperscan
 from autonomio.transform.transform_data import transform_data
-from autonomio.load_data import load_data
 from autonomio.transform.col_name_generator import col_name_generator
 from autonomio.transform.nan_imputer import nan_imputer
 from autonomio.plots.scatterz import scatterz
@@ -44,9 +43,11 @@ temp2.to_csv('test_data.csv')
 data('test_data.csv', 'file', header=None)
 data('test_data.csv')
 
-temp1 = wrangler(df=temp,y='neg', vectorize='text')
-temp1 = wrangler(df=temp,max_categories='max',to_string='text',first_fill_cols='url',starts_with_col='location')
-temp1 = wrangler(df=temp,max_categories=42, vectorize=['text', 'user_tweets'])
+temp1 = wrangler(df=temp, y='neg', vectorize='text')
+temp1 = wrangler(df=temp, max_categories='max', to_string='text',
+                 first_fill_cols='url', starts_with_col='location')
+temp1 = wrangler(df=temp, max_categories=42,
+                 vectorize=['text', 'user_tweets'])
 
 X = transform_data(temp, flatten='none')
 X = transform_data(temp, flatten='none', X=1)
@@ -54,26 +55,38 @@ Y = transform_data(temp, flatten='none', Y='neg')
 X, Y = transform_data(temp, flatten='none', X=1, Y='neg')
 
 # x variable input modes
-tr = train(1,'neg',temp,model='regression',flatten='mode')
-tr = train([1,5],'neg',temp,model='regression',reg_mode='logistic',flatten='cat_string')
-tr = train([1,2,3,4,5],'neg',temp,model='regression',reg_mode='regularized',flatten='cat_numeric')
+tr = train(1, 'neg', temp, model='regression', flatten='mode')
+tr = train([1, 5], 'neg', temp, model='regression',
+           reg_mode='logistic', flatten='cat_string')
+tr = train([1, 2, 3, 4, 5], 'neg', temp,
+           model='regression',
+           reg_mode='regularized',
+           flatten='cat_numeric')
 
 # y variable flattening mode
-tr = train(1,'quality_score',temp,flatten='median')
-tr = train(1,'quality_score',temp,flatten=6)
-tr = train(1,'quality_score',temp,flatten=.5)
-tr = train(1,'quality_score',temp,flatten='mean')
+tr = train(1, 'quality_score', temp, flatten='median')
+tr = train(1, 'quality_score', temp, flatten=6)
+tr = train(1, 'quality_score', temp, flatten=.5)
+tr = train(1, 'quality_score', temp, flatten='mean')
 
 # model saving and loading
-tr = train('text','neg',temp,save_model='test_model')
-te = predictor(temp,'test_model')
+tr = train('text', 'neg', temp, save_model='test_model')
+te = predictor(temp, 'test_model')
 
-#for validation
+# for validation
 tr = train(1, 'neg', temp, layers=1, validation=True)
 tr = train(1, 'neg', temp, validation=.6)
 
-tr = train(['reach_score','influence_score'],'neg',temp,save_model='strings')
-te = predictor(temp,'strings',x_plot='influence_score',y_plot='user_followers',labels='handle')
+tr = train(['reach_score', 'influence_score'],
+           'neg',
+           temp,
+           save_model='strings')
+
+te = predictor(temp,
+               'strings',
+               interactive=True,
+               interactive_x='user_followers',
+               label='handle')
 
 train()
 train(model='lstm')
@@ -89,25 +102,26 @@ l = ['funnel',
 
 for i in l:
 
-    if i in (l[0:3]): #funnel, brick, triangle
+    if i in (l[0:3]):  # funnel, brick, triangle
         tr = train(1, 'neg', temp, shape=i, double_check=True)
 
-    elif i in (l[3:4]): #only rhombus
+    elif i in (l[3:4]):  # only rhombus
         tr = train(1, 'neg', temp, shape=i, neuron_max=1, layers=8)
         tr = train(1, 'neg', temp, shape=i, neuron_max=9, layers=4)
 
-    if i in (l[4:6]): #long_funnel, diamond
+    if i in (l[4:6]):  # long_funnel, diamond
         tr = train(1, 'neg', temp, shape=i, layers=6)
         tr = train(1, 'neg', temp, shape=i, layers=5)
 
-    elif i in (l[6:8]): #hexagon, stairs
+    elif i in (l[6:8]):  # hexagon, stairs
         tr = train(1, 'neg', temp, shape=i, layers=4)
         tr = train(1, 'neg', temp, shape=i, layers=6)
         tr = train(1, 'neg', temp, shape=i, layers=7)
         tr = train(1, 'neg', temp, shape=i, layers=8)
 
 scatterz('influence_score', 'neg', temp, labels='handle')
-scatterz('influence_score', 'neg', temp,labels='handle',yscale='log',xscale='log')
+scatterz('influence_score', 'neg', temp,
+         labels='handle', yscale='log', xscale='log')
 
 hyperscan('user_followers', 'neg', temp.head(10), epochs=3,
           batch_sizes=[5, 6],
@@ -156,7 +170,7 @@ df = wrangler(temp, y='Survived',
               starts_with_col='Cabin',
               treshold=.8)
 
-x = train([2,3,4,5,6,7,8,9],'Survived',df,
+x = train([2, 3, 4, 5, 6, 7, 8, 9], 'Survived', df,
           flatten='none',
           epoch=150,
           dropout=0,
@@ -173,7 +187,7 @@ paramscatter(x[1], 'train_acc', limit=5)
 paramagg(x[1])
 paramgrid(x[1], 'train_acc')
 
-x2 = train([2, 3, 4, 5, 6, 7, 8, 9],'Survived', df,
+x2 = train([2, 3, 4, 5, 6, 7, 8, 9], 'Survived', df,
            flatten='none',
            epoch=3,
            dropout=0,
@@ -193,6 +207,6 @@ if p < .8:
     1/0
 
 temp = data('parties_and_employment')
-#test for lstm model
+# test for lstm model
 train(temp.MUU, epoch=1, batch_size=512, model='lstm', normalize_window=False)
 train(temp.MUU, epoch=1, batch_size=512, model='lstm', normalize_window=True)
