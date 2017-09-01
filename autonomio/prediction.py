5173c2b07471b213786e371d80ffe5fb3c669221
konchokdolma <aanastasia.gromova@gmail.com>
2017-09-01 19:31:31 +0300

    Updating prediction file

--- autonomio/prediction.py
+++ autonomio/prediction.py
@@ -2,32 +2,33 @@ import pandas as pd
 
 from autonomio.transform.transform_data import transform_data
 from autonomio.plots.plots import prediction_distribution
-from autonomio.plots.scatterz import scatterz
 from autonomio.load_model import load_model
 
 from IPython.display import display
-import mpld3
-
-mpld3.enable_notebook()
 
 def make_prediction(data,
                     saved_model,
-                    label,
+                    labels,
                     interactive,
                     interactive_x):
+
+    '''Predictor
+    WHAT: Makes predictions on a given dataset based on a saved
+    model that have been previously trained with train().
+    '''
 
     loaded_model, X, flatten = load_model(saved_model)
     signals = transform_data(data, flatten, X)
     prediction = loaded_model.predict(signals)
 
-    if label is False:
+    if labels is False:
         prediction = pd.DataFrame(prediction)
         prediction.columns = ['Prediction']
 
     else:
         prediction = pd.DataFrame(prediction)
-        prediction[label] = data[label]
-        prediction.columns = ['Prediction', label]
+        prediction[labels] = data[labels]
+        prediction.columns = ['Prediction', labels]
 
     prediction = prediction.sort_values('Prediction', ascending=False)
 
@@ -56,8 +57,8 @@ def make_prediction(data,
 
         temp = pd.merge(prediction,
                         data,
-                        left_on=label,
-                        right_on=label)
+                        left_on=labels,
+                        right_on=labels)
         return temp
 
     else:
