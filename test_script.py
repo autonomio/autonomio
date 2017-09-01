aba184e41800676f2609f51ab30fd7b16979b421
konchokdolma <aanastasia.gromova@gmail.com>
2017-09-01 21:25:01 +0300

    fixed to string for wrangler

--- test_script.py
+++ test_script.py
@@ -1,12 +1,12 @@
 from autonomio.commands import data, train, predictor, wrangler, hyperscan
 from autonomio.transform.transform_data import transform_data
-from autonomio.load_data import load_data
 from autonomio.transform.col_name_generator import col_name_generator
 from autonomio.transform.nan_imputer import nan_imputer
 from autonomio.plots.scatterz import scatterz
 from autonomio.hyperparameters import load_parameters
 from autonomio.hyperstats import hyper_descriptive
 from autonomio.transform.onehot_encoding import onehot
+from autonomio.transform.sohot_encoding import all_is_binary
 from autonomio.plots.duaparam import duaparam
 from autonomio.plots.paramagg import paramagg
 from autonomio.plots.quadparam import quadparam
@@ -29,24 +29,26 @@ temp = data('random_tweets')
 temp = temp.head(100)
 
 temp.to_msgpack('test_data.msgpack')
-data('test_data.msgpack','file')
+data('test_data.msgpack', 'file')
 
 temp.to_json('test_data.json')
-data('test_data.json','file')
+data('test_data.json', 'file')
 
 temp2 = np.array(temp)
-temp2 = temp2[:,2:4]
+temp2 = temp2[:, 2:4]
 temp2 = pd.DataFrame(temp2)
 
 data('temp.x', 'file')
 
 temp2.to_csv('test_data.csv')
-data('test_data.csv','file', header=None)
+data('test_data.csv', 'file', header=None)
 data('test_data.csv')
 
-temp1 = wrangler(df=temp,y='neg', vectorize='text')
-temp1 = wrangler(df=temp,max_categories='max',to_string='text',first_fill_cols='url',starts_with_col='location')
-temp1 = wrangler(df=temp,max_categories=42, vectorize=['text', 'user_tweets'])
+temp1 = wrangler(df=temp, y='neg', vectorize='text')
+temp1 = wrangler(df=temp, max_categories='max', to_string='text',
+                 first_fill_cols='url', starts_with_col='location')
+temp1 = wrangler(df=temp, max_categories=42,
+                 vectorize=['text', 'user_tweets'])
 
 X = transform_data(temp, flatten='none')
 X = transform_data(temp, flatten='none', X=1)
@@ -54,26 +56,38 @@ Y = transform_data(temp, flatten='none', Y='neg')
 X, Y = transform_data(temp, flatten='none', X=1, Y='neg')
 
 # x variable input modes
-tr = train(1,'neg',temp,model='regression',flatten='mode')
-tr = train([1,5],'neg',temp,model='regression',reg_mode='logistic',flatten='cat_string')
-tr = train([1,2,3,4,5],'neg',temp,model='regression',reg_mode='regularized',flatten='cat_numeric')
+tr = train(1, 'neg', temp, model='regression', flatten='mode')
+tr = train([1, 5], 'neg', temp, model='regression',
+           reg_mode='logistic', flatten='cat_string')
+tr = train([1, 2, 3, 4, 5], 'neg', temp,
+           model='regression',
+           reg_mode='regularized',
+           flatten='cat_numeric')
 
 # y variable flattening mode
-tr = train(1,'quality_score',temp,flatten='median')
-tr = train(1,'quality_score',temp,flatten=6)
-tr = train(1,'quality_score',temp,flatten=.5)
-tr = train(1,'quality_score',temp,flatten='mean')
+tr = train(1, 'quality_score', temp, flatten='median')
+tr = train(1, 'quality_score', temp, flatten=6)
+tr = train(1, 'quality_score', temp, flatten=.5)
+tr = train(1, 'quality_score', temp, flatten='mean')
 
 # model saving and loading
-tr = train('text','neg',temp,save_model='test_model')
-te = predictor(temp,'test_model')
+tr = train('text', 'neg', temp, save_model='test_model')
+te = predictor(temp, 'test_model')
 
-#for validation
+# for validation
 tr = train(1, 'neg', temp, layers=1, validation=True)
 tr = train(1, 'neg', temp, validation=.6)
 
-tr = train(['reach_score','influence_score'],'neg',temp,save_model='strings')
-te = predictor(temp,'strings',x_plot='influence_score',y_plot='user_followers',labels='handle')
+tr = train(['reach_score', 'influence_score'],
+           'neg',
+           temp,
+           save_model='strings')
+
+te = predictor(temp,
+               'strings',
+               interactive=True,
+               interactive_x='user_followers',
+               labels='handle')
 
 train()
 train(model='lstm')
@@ -89,62 +103,59 @@ l = ['funnel',
 
 for i in l:
 
-    if i in (l[0:3]): #funnel, brick, triangle
+    if i in (l[0:3]):  # funnel, brick, triangle
         tr = train(1, 'neg', temp, shape=i, double_check=True)
 
-    elif i in (l[3:4]): #only rhombus
+    elif i in (l[3:4]):  # only rhombus
         tr = train(1, 'neg', temp, shape=i, neuron_max=1, layers=8)
         tr = train(1, 'neg', temp, shape=i, neuron_max=9, layers=4)
 
-    if i in (l[4:6]): #long_funnel, diamond
+    if i in (l[4:6]):  # long_funnel, diamond
         tr = train(1, 'neg', temp, shape=i, layers=6)
         tr = train(1, 'neg', temp, shape=i, layers=5)
 
-    elif i in (l[6:8]): #hexagon, stairs
+    elif i in (l[6:8]):  # hexagon, stairs
         tr = train(1, 'neg', temp, shape=i, layers=4)
         tr = train(1, 'neg', temp, shape=i, layers=6)
         tr = train(1, 'neg', temp, shape=i, layers=7)
         tr = train(1, 'neg', temp, shape=i, layers=8)
 
 scatterz('influence_score', 'neg', temp, labels='handle')
-scatterz('influence_score', 'neg', temp,labels='handle',yscale='log',xscale='log')
+scatterz('influence_score', 'neg', temp,
+         labels='handle', yscale='log', xscale='log')
 
 hyperscan('user_followers', 'neg', temp.head(10), epochs=3,
-                                                  batch_sizes=[5,6],
-                                                  scan_mode='selective',
-                                                  losses='logcosh',
-                                                  optimizers='adam', 
-                                                  activations='elu')
+          batch_sizes=[5, 6],
+          losses='logcosh',
+          optimizers='adam',
+          activations='elu')
 
 hyperscan('user_followers', 'neg', temp.head(10), epochs=3,
-                                                  layers=[5,6],
-                                                  scan_mode='selective',
-                                                  losses='logcosh',
-                                                  optimizers='adam',
-                                                  shapes='funnel')
+          layers=[5, 6],
+          losses='logcosh',
+          optimizers='adam',
+          shapes='funnel')
 
 hyperscan('user_followers', 'neg', temp.head(10), epochs=3,
-                                                  scan_mode='selective',
-                                                  losses='logcosh',
-                                                  activations='elu',
-                                                  shapes='funnel')
+          losses='logcosh',
+          activations='elu',
+          shapes='funnel')
 
 hyperscan('user_followers', 'neg', temp.head(10), epochs=3,
-                                                  scan_mode='selective',
-                                                  optimizers='adam', 
-                                                  activations='elu',
-                                                  shapes='funnel')
+          optimizers='adam',
+          activations='elu',
+          shapes='funnel')
 
 load_parameters('categorical_losses')
 
-#check for a good result
+# check for a good result
 temp = data('kaggle_titanic_train')
 
 a = hyper_descriptive(temp, 'Age', 'SibSp')
-a = hyper_descriptive(temp, 'Age', 'SibSp', mode = 'mean')
-a = hyper_descriptive(temp, 'Age', 'SibSp', mode = 'std')
-a = hyper_descriptive(temp, 'Age', 'SibSp', mode = 'min')
-a = hyper_descriptive(temp, ['Age', 'Survived'], 'SibSp', mode = 'max')
+a = hyper_descriptive(temp, 'Age', 'SibSp', mode='mean')
+a = hyper_descriptive(temp, 'Age', 'SibSp', mode='std')
+a = hyper_descriptive(temp, 'Age', 'SibSp', mode='min')
+a = hyper_descriptive(temp, ['Age', 'Survived'], 'SibSp', mode='max')
 
 b = nan_imputer(temp.Age)
 b = nan_imputer(temp.Age, mode='mean')
@@ -152,44 +163,53 @@ b = nan_imputer(temp.Age, mode='median')
 b = nan_imputer(temp.Age, mode='common')
 b = nan_imputer(temp.Age, mode='mode')
 
+c = all_is_binary(df, 'Survived')
+
 l = temp.Parch.tolist()
 a = onehot(l)
 
-df = wrangler(temp,y='Survived',
-                   first_fill_cols='Cabin',
-                   starts_with_col='Cabin',
-                   treshold=.8)
+df = wrangler(temp, y='Survived',
+              first_fill_cols='Cabin',
+              starts_with_col='Cabin',
+              treshold=.8)
 
-x = train([2,3,4,5,6,7,8,9],'Survived',df,
-                        flatten='none',
-                        epoch=150,
-                        dropout=0,
-                        batch_size=12,
-                        loss='logcosh',
-                        activation='elu',
-                        layers=6,
-                        shape='brick',
-                        hyperscan=True)
+x = train([2, 3, 4, 5, 6, 7, 8, 9], 'Survived', df,
+          flatten='none',
+          epoch=150,
+          dropout=0,
+          batch_size=12,
+          loss='logcosh',
+          activation='elu',
+          layers=6,
+          shape='brick',
+          hyperscan=True)
 
 quadparam(x[1], 'test_acc', 'test_loss', 'train_acc', 'train_loss')
 duaparam(x[1], 'test_acc', 80, 70)
-paramscatter(x[1], 'train_acc', limit = 5)
+paramscatter(x[1], 'train_acc', limit=5)
 paramagg(x[1])
 paramgrid(x[1], 'train_acc')
 
-x2 = train([2,3,4,5,6,7,8,9],'Survived',df,
-                        flatten='none',
-                        epoch=3,
-                        dropout=0,
-                        batch_size=12,
-                        loss='logcosh',
-                        activation='elu',
-                        layers=6,
-                        shape='brick',
-                        hyperscan=True)
+x2 = train([2, 3, 4, 5, 6, 7, 8, 9], 'Survived', df,
+           flatten='none',
+           epoch=3,
+           dropout=0,
+           batch_size=12,
+           loss='logcosh',
+           activation='elu',
+           layers=6,
+           shape='brick',
+           hyperscan=True)
 
 paramscatter(x2[1], 'train_acc', sort=False)
 paramgrid(x2[1], 'train_acc')
+
+temp2 = pd.DataFrame(['0', '0', '0'], columns=['0'])
+
+try:
+    paramgrid(temp2, '0')
+except:
+    pass
 
 p = x[1][-10:]['train_acc'].mean()
 if p < .8:
@@ -197,6 +217,6 @@ if p < .8:
     1/0
 
 temp = data('parties_and_employment')
-#test for lstm model
+# test for lstm model
 train(temp.MUU, epoch=1, batch_size=512, model='lstm', normalize_window=False)
 train(temp.MUU, epoch=1, batch_size=512, model='lstm', normalize_window=True)
