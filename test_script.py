16c9ac6329b00ea6eb947e398c462cfbae2b0ae4
konchokdolma <aanastasia.gromova@gmail.com>
2017-09-03 19:11:58 +0300

    Updated test script

--- test_script.py
+++ test_script.py
@@ -17,23 +17,35 @@ from autonomio.plots.paramgrid import paramgrid
 import pandas as pd
 import numpy as np
 
-# datasets
+# TABLE OF CONTENTS
+# 1) data                line 31
+# 2) wrangler            line 60
+# 3) train               line 76
+# 4) predictor           line 106
+# 5) shapes              line 114
+# 6) hyperscan           line 145
+# 7) text_transformers   line 169
+# 8) plots               line 187
+# 9) helper scripts      line 210
+
+# 1) data
 temp = data('tweet_sentiment')
 temp = data('election_in_twitter')
 temp = data('sites_category_and_vec')
 temp = data('bitcoin_price')
 temp = data('programmatic_ad_fraud')
-temp = col_name_generator(pd.DataFrame([1, 2]))
-
-# create dataset for rest of the tests
 temp = data('random_tweets')
-temp = temp.head(100)
+
+temp_employment = data('parties_and_employment')
+temp_titanic = data('kaggle_titanic_train')
 
 temp.to_msgpack('test_data.msgpack')
 data('test_data.msgpack', 'file')
 
 temp.to_json('test_data.json')
 data('test_data.json', 'file')
+
+temp = temp.head(100)
 
 temp2 = np.array(temp)
 temp2 = temp2[:, 2:4]
@@ -45,6 +57,10 @@ temp2.to_csv('test_data.csv')
 data('test_data.csv', 'file', header=None)
 data('test_data.csv')
 
+# 2) wrangler
+
+temp['datetime'] = temp['created_at']
+
 temp1 = wrangler(data=temp,
                  y='neg',
                  vectorize='text')
@@ -53,18 +69,15 @@ temp1 = wrangler(data=temp,
                  max_categories='max',
                  to_string='text',
                  fill_columns='url',
-                 starts_with_col='location')
+                 starts_with_col='location',
+                 datetime_mode='sequence')
 
 temp1 = wrangler(data=temp,
                  max_categories=42,
-                 vectorize=['text', 'user_tweets'])
+                 vectorize=['text', 'user_tweets'],
+                 datetime_mode='drop')
 
-X = transform_data(temp, flatten='none')
-X = transform_data(temp, flatten='none', X=1)
-Y = transform_data(temp, flatten='none', Y='neg')
-X, Y = transform_data(temp, flatten='none', X=1, Y='neg')
-
-# x variable input modes
+# 3) train
 tr = train(1, 'neg', temp, model='regression', flatten='mode')
 tr = train([1, 5], 'neg', temp, model='regression',
            reg_mode='logistic', flatten='cat_string')
@@ -73,24 +86,37 @@ tr = train([1, 2, 3, 4, 5], 'neg', temp,
            reg_mode='regularized',
            flatten='cat_numeric')
 
-# y variable flattening mode
 tr = train(1, 'quality_score', temp, flatten='median')
 tr = train(1, 'quality_score', temp, flatten=6)
 tr = train(1, 'quality_score', temp, flatten=.5)
 tr = train(1, 'quality_score', temp, flatten='mean')
 
-# model saving and loading
 tr = train('text', 'neg', temp, save_model='test_model')
 te = predictor(temp, 'test_model')
 
-# for validation
 tr = train(1, 'neg', temp, layers=1, validation=True)
-tr = train(1, 'neg', temp, validation=.6)
+tr = train(1, 'neg', temp, validation=.6, shape_plot=True)
+
+train(temp_employment.MUU,
+      epoch=1,
+      batch_size=512,
+      model='lstm',
+      normalize_window=False)
+train(temp_employment.MUU,
+      epoch=1,
+      batch_size=512,
+      model='lstm',
+      normalize_window=True)
 
 tr = train(['reach_score', 'influence_score'],
            'neg',
            temp,
            save_model='strings')
+
+train()
+train(model='lstm')
+
+# 4) predicton
 
 te = predictor(temp,
                'strings',
@@ -98,12 +124,12 @@ te = predictor(temp,
                interactive_x='user_followers',
                labels='handle')
 
-train()
-train(model='lstm')
+# 5) shapes
 
 l = ['funnel',
      'brick',
      'triangle',
+     'pyramid',
      'rhombus',
      'long_funnel',
      'diamond',
@@ -112,26 +138,24 @@ l = ['funnel',
 
 for i in l:
 
-    if i in (l[0:3]):  # funnel, brick, triangle
-        tr = train(1, 'neg', temp, shape=i, double_check=True)
+    if i in (l[0:4]):
+        tr = train(1, 'neg', temp, shape=i, double_check=True, layers=15)
 
-    elif i in (l[3:4]):  # only rhombus
+    elif i in (l[4:5]):
         tr = train(1, 'neg', temp, shape=i, neuron_max=1, layers=8)
         tr = train(1, 'neg', temp, shape=i, neuron_max=9, layers=4)
 
-    if i in (l[4:6]):  # long_funnel, diamond
-        tr = train(1, 'neg', temp, shape=i, layers=6)
-        tr = train(1, 'neg', temp, shape=i, layers=5)
+    if i in (l[5:7]):
+        tr = train(1, 'neg', temp, shape=i, layers=13)
+        tr = train(1, 'neg', temp, shape=i, layers=14)
 
-    elif i in (l[6:8]):  # hexagon, stairs
+    elif i in (l[7:9]):
         tr = train(1, 'neg', temp, shape=i, layers=4)
-        tr = train(1, 'neg', temp, shape=i, layers=6)
-        tr = train(1, 'neg', temp, shape=i, layers=7)
-        tr = train(1, 'neg', temp, shape=i, layers=8)
+        tr = train(1, 'neg', temp, shape=i, layers=10)
+        tr = train(1, 'neg', temp, shape=i, layers=11)
+        tr = train(1, 'neg', temp, shape=i, layers=12)
 
-scatterz('influence_score', 'neg', temp, labels='handle')
-scatterz('influence_score', 'neg', temp,
-         labels='handle', yscale='log', xscale='log')
+# 6) hyperscan
 
 hyperscan('user_followers', 'neg', temp.head(10), epochs=3,
           batch_sizes=[5, 6],
@@ -155,69 +179,58 @@ hyperscan('user_followers', 'neg', temp.head(10), epochs=3,
           activations='elu',
           shapes='funnel')
 
-load_parameters('categorical_losses')
+# 7) text_transformers
 
-# check for a good result
-temp = data('kaggle_titanic_train')
+X = transform_data(temp, flatten='none')
+X = transform_data(temp, flatten='none', X=1)
+Y = transform_data(temp, flatten='none', Y='neg')
+X, Y = transform_data(temp, flatten='none', X=1, Y='neg')
 
-a = hyper_descriptive(temp, 'Age', 'SibSp')
-a = hyper_descriptive(temp, 'Age', 'SibSp', mode='mean')
-a = hyper_descriptive(temp, 'Age', 'SibSp', mode='std')
-a = hyper_descriptive(temp, 'Age', 'SibSp', mode='min')
-a = hyper_descriptive(temp, ['Age', 'Survived'], 'SibSp', mode='max')
+b = nan_imputer(temp_titanic.Age)
+b = nan_imputer(temp_titanic.Age, impute_mode='mean')
+b = nan_imputer(temp_titanic.Age, impute_mode='median')
+b = nan_imputer(temp_titanic.Age, impute_mode='common')
+b = nan_imputer(temp_titanic.Age, impute_mode='mode')
 
-b = nan_imputer(temp.Age)
-b = nan_imputer(temp.Age, impute_mode='mean')
-b = nan_imputer(temp.Age, impute_mode='median')
-b = nan_imputer(temp.Age, impute_mode='common')
-b = nan_imputer(temp.Age, impute_mode='mode')
+c = all_is_binary(temp_titanic, 'Survived')
 
-c = all_is_binary(temp, 'Survived')
-
-l = temp.Parch.tolist()
+l = temp_titanic.Parch.tolist()
 a = onehot(l)
 
-df = wrangler(temp, y='Survived',
-              fill_columns='Cabin',
-              starts_with_col='Cabin',
-              nan_treshold=.8)
+temp3 = col_name_generator(pd.DataFrame([1, 2]))
 
-x = train([2, 3, 4, 5, 6, 7], 'Survived', df,
-          flatten='none',
-          epoch=150,
-          dropout=0,
-          batch_size=12,
-          loss='logcosh',
-          activation='elu',
-          layers=6,
-          shape='brick',
-          hyperscan=True)
+# 8) plots
+
+scatterz('influence_score', 'neg', temp, labels='handle')
+scatterz('influence_score', 'neg', temp,
+         labels='handle', yscale='log', xscale='log')
+
+x = train(2, 'Survived', temp_titanic, flatten='none', hyperscan=True)
 
 quadparam(x[1], 'test_acc', 'test_loss', 'train_acc', 'train_loss')
 duaparam(x[1], 'test_acc', 80, 70)
 paramscatter(x[1], 'train_acc', limit=5)
 paramagg(x[1])
 paramgrid(x[1], 'train_acc')
-# paramscatter(x[1], 'train_acc', sort=False)
+paramscatter(x[1], 'train_acc', sort=False)
 paramgrid(x[1], 'train_acc')
 
 temp2 = pd.DataFrame(['0', '0', '0'], columns=['0'])
 
-'''
 try:
     paramgrid(temp2, '0')
 except:
     pass
-p = x[1][-10:]['train_acc'].mean()
-if p < .8:
-    print('bad result for titanic data')
-    1/0
-'''
 
-temp = data('parties_and_employment')
-# test for lstm model
-train(temp.MUU, epoch=1, batch_size=512, model='lstm', normalize_window=False)
-train(temp.MUU, epoch=1, batch_size=512, model='lstm', normalize_window=True)
+# 9) helper scripts
 
 max_rescale([0, 1, 2], to_int=True)
 max_rescale([0, 1, 2])
+
+a = hyper_descriptive(temp_titanic, 'Age', 'SibSp')
+a = hyper_descriptive(temp_titanic, 'Age', 'SibSp', mode='mean')
+a = hyper_descriptive(temp_titanic, 'Age', 'SibSp', mode='std')
+a = hyper_descriptive(temp_titanic, 'Age', 'SibSp', mode='min')
+a = hyper_descriptive(temp_titanic, ['Age', 'Survived'], 'SibSp', mode='max')
+
+load_parameters('categorical_losses')
